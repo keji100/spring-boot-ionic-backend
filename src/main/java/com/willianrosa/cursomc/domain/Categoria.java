d8bf524f832e49dc99ce8f7905b5ebcda469fd84
Willian Rosa <willianjapa100@gmail.com>
2018-07-07 21:10:46 -0300

    Banco de dados H2 e criacao automatica da base de dados

--- src/main/java/com/willianrosa/cursomc/domain/Categoria.java
+++ src/main/java/com/willianrosa/cursomc/domain/Categoria.java
@@ -1,10 +1,17 @@
-package curso.willianrosa.cursomc.domain;
+package com.willianrosa.cursomc.domain;
 import java.io.Serializable;
 
-public class Categoria implements Serializable{
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
+@Entity
+public class Categoria implements Serializable{
 	private static final long serialVersionUID = 1L;
 
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private Integer id;
 	private String nome;
 	
--- src/main/java/com/willianrosa/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/willianrosa/cursomc/resources/CategoriaResource.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import curso.willianrosa.cursomc.domain.Categoria;
+import com.willianrosa.cursomc.domain.Categoria;
 
 @RestController
 @RequestMapping(value="/categoria")
