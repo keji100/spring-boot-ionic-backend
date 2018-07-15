ed1939e424efe6f0fe9425d643c22d5ca335d212
Willian Rosa <willianjapa100@gmail.com>
2018-07-15 20:16:46 -0300

    ItemPedido e ItemPedidoPK

--- src/main/java/com/willianrosa/cursomc/CursomcApplication.java
+++ src/main/java/com/willianrosa/cursomc/CursomcApplication.java
@@ -14,6 +14,7 @@ import com.willianrosa.cursomc.domain.Cidade;
 import com.willianrosa.cursomc.domain.Cliente;
 import com.willianrosa.cursomc.domain.Endereco;
 import com.willianrosa.cursomc.domain.Estado;
+import com.willianrosa.cursomc.domain.ItemPedido;
 import com.willianrosa.cursomc.domain.Pagamento;
 import com.willianrosa.cursomc.domain.PagamentoComBoleto;
 import com.willianrosa.cursomc.domain.PagamentoComCartao;
@@ -26,6 +27,7 @@ import com.willianrosa.cursomc.repositories.CidadeRepository;
 import com.willianrosa.cursomc.repositories.ClienteRepository;
 import com.willianrosa.cursomc.repositories.EnderecoRepository;
 import com.willianrosa.cursomc.repositories.EstadoRepository;
+import com.willianrosa.cursomc.repositories.ItemPedidoRepository;
 import com.willianrosa.cursomc.repositories.PagamentoRepository;
 import com.willianrosa.cursomc.repositories.PedidoRepository;
 import com.willianrosa.cursomc.repositories.ProdutoRepository;
@@ -49,6 +51,8 @@ public class CursomcApplication implements CommandLineRunner{
 	private PedidoRepository pedidoRepository;
 	@Autowired
 	private PagamentoRepository pagamentoRepository;
+	@Autowired
+	private ItemPedidoRepository itemPedidoRepository;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
@@ -115,5 +119,17 @@ public class CursomcApplication implements CommandLineRunner{
 		pedidoRepository.saveAll(Arrays.asList(ped1,ped2));
 		pagamentoRepository.saveAll(Arrays.asList(pagto1,pagto2));
 		
+		ItemPedido ip1 = new ItemPedido(ped1, p1, 0.00, 1, 2000.00);
+		ItemPedido ip2 = new ItemPedido(ped1, p3, 0.00, 2, 80.00);
+		ItemPedido ip3 = new ItemPedido(ped2, p2, 100.00, 1, 800.00);
+		
+		ped1.getItens().addAll(Arrays.asList(ip1,ip2));
+		ped2.getItens().addAll(Arrays.asList(ip3));
+		
+		p1.getItens().addAll(Arrays.asList(ip1));
+		p2.getItens().addAll(Arrays.asList(ip3));
+		p3.getItens().addAll(Arrays.asList(ip2));
+		
+		itemPedidoRepository.saveAll(Arrays.asList(ip1,ip2,ip3));
 	} 
 }
--- src/main/java/com/willianrosa/cursomc/domain/Pedido.java
+++ src/main/java/com/willianrosa/cursomc/domain/Pedido.java
@@ -2,16 +2,17 @@ package com.willianrosa.cursomc.domain;
 
 import java.io.Serializable;
 import java.util.Date;
+import java.util.HashSet;
+import java.util.Set;
 
 import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.Inheritance;
-import javax.persistence.InheritanceType;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
 
 @Entity
@@ -34,6 +35,9 @@ public class Pedido  implements Serializable{
 	@JoinColumn(name="endereco_entrega_id")
 	private Endereco enderecoDeEntrega;
 	
+	@OneToMany(mappedBy="id.pedido")
+ 	private Set<ItemPedido> itens = new HashSet<>();
+ 	
 	public Pedido() {
 		// TODO Auto-generated constructor stub
 	}
@@ -45,7 +49,7 @@ public class Pedido  implements Serializable{
 		this.cliente = cliente;
 		this.enderecoDeEntrega = enderecoDeEntrega;
 	}
-
+	
 	public Integer getId() {
 		return id;
 	}
@@ -84,6 +88,39 @@ public class Pedido  implements Serializable{
 
 	public void setEnderecoDeEntrega(Endereco enderecoDeEntrega) {
 		this.enderecoDeEntrega = enderecoDeEntrega;
+	}
+	
+	public Set<ItemPedido> getItens() {
+		return itens;
+	}
+	
+	public void setItens(Set<ItemPedido> itens) {
+		this.itens = itens;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Pedido other = (Pedido) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
 	}
 	
 }
