f744c22bf56de9ac00f0c2c7d21f95d146ed9f99
Willian Rosa <willianjapa100@gmail.com>
2018-09-30 23:44:42 -0300

    PUT, DELETE e GET para Cliente

---
+++ src/main/java/com/willianrosa/cursomc/dto/ClienteDTO.java
@@ -0,0 +1,57 @@
+package com.willianrosa.cursomc.dto;
+
+import java.io.Serializable;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+
+import org.hibernate.validator.constraints.Length;
+
+import com.willianrosa.cursomc.domain.Cliente;
+
+public class ClienteDTO implements Serializable{
+	private static final long serialVersionUID = 1L;
+
+	@NotEmpty(message="Preenchimento obrigatório")
+	@Length(min=5, max=120, message="O tamanho deve ser entre 5 e 120 caracteres")
+	private String nome;
+	
+	@NotEmpty(message="Preenchimento obrigatório")
+	@Email(message="Email inválido")
+	private String email;
+	private int id;
+
+	public ClienteDTO() {}
+	
+	public ClienteDTO(Cliente obj) {
+		id = obj.getId();
+		nome = obj.getNome();
+		email = obj.getEmail();
+	}
+	
+	public String getNome() {
+		return nome;
+	}
+	
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	public String getEmail() {
+		return email;
+	}
+	
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	public int getId() {
+		return id;
+	}
+	
+	public void setId(int id) {
+		this.id = id;
+	}
+	
+	
+}
--- src/main/java/com/willianrosa/cursomc/services/ClienteService.java
+++ src/main/java/com/willianrosa/cursomc/services/ClienteService.java
@@ -1,13 +1,20 @@
 package com.willianrosa.cursomc.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
-import com.willianrosa.cursomc.domain.Categoria;
 import com.willianrosa.cursomc.domain.Cliente;
+import com.willianrosa.cursomc.domain.Cliente;
+import com.willianrosa.cursomc.dto.ClienteDTO;
 import com.willianrosa.cursomc.repositories.ClienteRepository;
+import com.willianrosa.cursomc.services.exception.DataIntegrityException;
 import com.willianrosa.cursomc.services.exception.ObjectNotFoundException;
 
 @Service
@@ -19,6 +26,39 @@ public class ClienteService {
 	public Cliente find(Integer id) {
 		Optional<Cliente> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException(
-				"Objeto não encontrado! Id: "+id+", Tipo: "+Categoria.class.getName()));
+				"Objeto não encontrado! Id: "+id+", Tipo: "+Cliente.class.getName()));
+	}
+	
+	public List<Cliente> findAll(){
+		return repo.findAll();
+	}
+	
+	public Page<Cliente> findPage(Integer page, Integer linesPerPage, String orderby, String direction){
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderby);
+		return repo.findAll(pageRequest);
+	}
+	
+	public Cliente update(Cliente obj) {
+		Cliente newObj = this.find(obj.getId());
+		updateData(newObj,obj);
+		return repo.save(newObj);
+	}
+	
+	public void delete(Integer id) {
+		find(id);
+		try {
+			repo.deleteById(id);
+		}catch (DataIntegrityViolationException e) {
+			throw new DataIntegrityException("Não foi possível excluir um cliente que possue pedidos.");
+		}
+	}
+	
+	public Cliente fromDTO(ClienteDTO dto) {
+		return new Cliente(dto.getId(),dto.getNome(),dto.getEmail(), null, null);
+	}
+	
+	private void updateData(Cliente newObj, Cliente obj) {
+		newObj.setNome(obj.getNome());
+		newObj.setEmail(obj.getEmail());
 	}
 }
